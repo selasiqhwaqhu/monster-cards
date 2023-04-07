e5f325097ca26c517a4f02913ba79bcda99410c7
selasiqhwaqhu <selasiqhwaqhu@gmail.com>
2023-04-07 23:34:21 +0000

    Fetch data from an external api in json format and  assign it to an empty array

--- src/App.js
+++ src/App.js
@@ -1,33 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import React,{ Component } from 'react';
+import React from 'react';
 
 
-class App extends Component{
+class App extends React.Component{
     constructor(){
         super();
         this.state ={
-            monsters:[
-                {
-                    id:'eir',
-                    name:'Dracula'
-                },
-                {
-                    id:'idn',
-                    name:'Frankenstein'
-                },
-                {
-                    id:'eig',
-                    name:'Zombie'
-                },
-            ]
+            monsters:[]
         }
     }
 
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users').then(Response=>Response.json())
+        .then(users=>this.setState({monsters:users})).then(users=>console.log(users));
+    }
     render(){
         return (
             <div className="App">
-                {this.state.monsters.map(monster=><h1 key={monster.id}>{monster.name}</h1>)}
+                {this.state.monsters.map(monster=>(<h1 key={monster.id}>{monster.name}</h1>))}
             </div>
     
             );
