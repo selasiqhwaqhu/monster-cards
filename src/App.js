c51ca57bd57d190bd34bf3d94039ef94c686a88c
selasiqhwaqhu <selasiqhwaqhu@gmail.com>
2023-04-14 23:08:51 +0000

    loading msg, throw and catch error msg if api fails

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import {  useState } from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
 import SearchBox  from './components/search-box/search-box.component';
@@ -9,17 +9,40 @@ import { useEffect } from 'react';
 function App() {
 
     const [monsters,setMonsters] = useState([])
-    const [searchField, setSearchField]  = useState('')      
+    const [searchField, setSearchField]  = useState('') 
+    const [loadPending,setLoadPending]= useState(null)
+    const [errorMsg,setErrorMsg] = useState(null)
         
 
 
     useEffect(()=>{
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then((res) => res.json())
-            .then((data)=>setMonsters(data))
-            console.log('monsters');
+        setLoadPending(true)
+        setTimeout(() => {
+            fetch('https://jsonplaceholder.typicode.com/user')
+                .then((res) => {
+                    if (!res.ok){
+                        throw Error('Could not fetch data for that resource');
+                    }
+                    
+                    return res.json()
+                })
+                .then((data)=>{
+                    setMonsters(data)
+                    setLoadPending(false)
+                    setErrorMsg(null) 
+                })
+                .catch((err)=>{
+                    setErrorMsg(err.message) 
+                    setLoadPending(false) 
+                    setMonsters([])
+                })
+                
+                    
+        }, 1000);
+            
+            
     
-    },[])
+    },[errorMsg])
    
     
 
@@ -33,8 +56,9 @@ function App() {
                     placeholder='Search Monsters' 
                     handleChange= {e=>setSearchField(e.target.value)}
                 />
-                
-                 <CardList listMonsters ={filteredSearch}/>
+                {loadPending && <p>loading ...</p>}
+                {errorMsg!==null && <p>{errorMsg} ...</p>}
+                 {monsters!==null && <CardList listMonsters ={filteredSearch}/>}
             </div>
     
             );
